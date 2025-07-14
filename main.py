16ef0c5c11e31284fb27939e3491e46732c619dc
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-07-14 19:14:21 +0530

    code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello from Python!")
+#tcs practing
+
+# give a no of N find out the sum of the first N natural numbers
+# i/p: N =5
+# O/P : 15
+# explanation: 1+2+3+4+5
+#code
+N= int(input("enter a number:"))
+sum = N*(N+1)//2
+print(f"sum of {N} natural number is: {sum}")
