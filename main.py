77a3fce5fd51197102de179c17bd7aa7cbe7d5d4
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-07-14 19:55:12 +0530

    code

--- main.py
+++ main.py
@@ -9,20 +9,32 @@
 # sum = N*(N+1)//2
 # print(f"sum of {N} natural number is: {sum}")
 
-
 # Palindrome check
 # I/P: N =121
 # O/P: Palindrome
 # ----CODE----
-def is_palindrome(N):
-    OG, rev_num = N,0
-    while N>0:
-        digit = N%10
-        rev_num = rev_num*10+ digit
-        N //= 10
-    return OG == rev_num
+# def is_palindrome(N):
+#     OG, rev_num = N,0
+#     while N>0:
+#         digit = N%10
+#         rev_num = rev_num*10+ digit
+#         N //= 10
+#     return OG == rev_num
 
-N= int(input("enter a number:"))
-print("is palindrome:", is_palindrome(N) )
+# N= int(input("enter a number:"))
+# print("is palindrome:", is_palindrome(N) )
 
 # For the reversing just remove the OG from the code and return only rev_num for optional add the print("reversed number:", rev_num ) like that it will show you the reversed integer
+
+# Factorial
+# I/P: 5
+# O/P: 120
+# ----CODE----
+def factorial(X):
+    fact =1
+    for i in range(2, X+1):
+        fact *= i
+    return fact
+
+X= int(input("enter a number:"))
+print("Factorial of", X, "is:", factorial(X))
