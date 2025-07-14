c584618e0b9f575205382d6d221296c6293c0a04
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-07-14 19:39:58 +0530

    code

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-#tcs practing
+#TCS practing
 
 # give a no of N find out the sum of the first N natural numbers
 # i/p: N =5
 # O/P : 15
 # explanation: 1+2+3+4+5
-#code
+# -----CODE-----
+# N= int(input("enter a number:"))
+# sum = N*(N+1)//2
+# print(f"sum of {N} natural number is: {sum}")
+
+
+# Palindrome check
+# I/P: N =121
+# O/P: Palindrome
+# ----CODE----
+def is_palindrome(N):
+    OG, rev_num = N,0
+    while N>0:
+        digit = N%10
+        rev_num = rev_num*10+ digit
+        N //= 10
+    return OG == rev_num
+
 N= int(input("enter a number:"))
-sum = N*(N+1)//2
-print(f"sum of {N} natural number is: {sum}")
+print("is palindrome:", is_palindrome(N) )
+
+# For the reversing just remove the OG from the code and return only rev_num for optional add the print("reversed number:", rev_num ) like that it will show you the reversed integer
