0f8e5d171ba51e2af46e2e70963a4b454108440a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-07-16 23:56:17 +0530

    code

--- main.py
+++ main.py
@@ -270,14 +270,13 @@
 # I/P: 5 1 4 2 3
 # O/P: 1 2 3 4 5
 # ----Code----
+# def bubblesort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
 
-def bubblesort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-
-arr = list(map(int,input().split()))
-print(bubblesort(arr))
+# arr = list(map(int,input().split()))
+# print(bubblesort(arr))
